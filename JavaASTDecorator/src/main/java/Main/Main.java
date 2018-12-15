a8e056d8f16a55bd49efc4a5ce860c7e7ae7b61a
dyhe83 <32974708+dyhe83@users.noreply.github.com>
2018-12-15 16:59:36 +0800

    輸出 serialized object 二進制檔
    輸出 serialized object 二進制檔

--- JavaASTDecorator/src/main/java/Main/Main.java
+++ JavaASTDecorator/src/main/java/Main/Main.java
@@ -8,6 +8,8 @@ import com.github.javaparser.serialization.JavaParserJsonSerializer;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 
 public class Main {
 
@@ -25,6 +27,13 @@ public class Main {
 
 			// write Json File
 			JsonOutputUtils.toJson(cu, "./src/main/resources/big-o ast.json");
+
+			// write serialized object
+			FileOutputStream fileOut = new FileOutputStream("./src/main/resources/big-o ast.ser");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(cu);
+			out.close();
+			fileOut.close();
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
