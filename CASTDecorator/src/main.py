acc852e3dfd5d1f87bf69777019a4b15e9e7f148
dyhe83 <32974708+dyhe83@users.noreply.github.com>
2019-02-20 16:58:37 +0800

    move 'big-o ast.json' to example folder

--- CASTDecorator/src/main.py
+++ CASTDecorator/src/main.py
@@ -3,13 +3,13 @@ from src.decorate_visitor import Decorator
 
 
 def main():
-    ast = ASTGenerator().generate('test/FullTest.c')
+    ast = ASTGenerator().generate('examples/FullTest.c')
 
     bigo_ast = Decorator().decorate(ast)
 
     print(bigo_ast.to_json())
 
-    f = open("test/big-o ast.json", "w")
+    f = open("examples/big-o ast.json", "w")
     f.write(bigo_ast.to_json())
     f.close()
 
