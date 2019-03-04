1f4360adf99ae1dec8fb21675021ab0fdc8617f7
dyhe83 <32974708+dyhe83@users.noreply.github.com>
2019-03-04 16:33:55 +0800

    Create bigo_ast_visitor.py

---
+++ CASTDecorator/BigOAst/bigo_ast_visitor.py
@@ -0,0 +1,22 @@
+from BigOAst.BigOAST import BasicNode
+
+
+class BigOAstVisitor(object):
+    _method_cache = None
+
+    def visit(self, node: BasicNode):
+
+        if self._method_cache is None:
+            self._method_cache = {}
+
+        visitor = self._method_cache.get(node.__class__.__name__, None)
+        if visitor is None:
+            method = 'visit_' + node.__class__.__name__
+            visitor = getattr(self, method, self.generic_visit)
+            self._method_cache[node.__class__.__name__] = visitor
+
+        return visitor(node)
+
+    def generic_visit(self, node):
+        for c in node:
+            self.visit(c)
