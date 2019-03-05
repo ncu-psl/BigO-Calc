eb5d25834a9fba2e5477529c78cdd640f1c1fda0
dyhe83 <dyhe83@gmail.com>
2019-03-05 11:26:55 +0800

    call BigoEval.eval()

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from CASTDecorator.src.ast_generator.ast_generator import ASTGenerator
 from CASTDecorator.src.ast_decorator.decorate_visitor import DecorateVisitor
+from CASTDecorator.src.ast_generator.ast_generator import ASTGenerator
+from bigo_calculator.bigo_evaluator import BigOEvaluator
 
 
 def main():
     ast = ASTGenerator().generate('CASTDecorator/examples/FullTest.c')
 
     bigo_ast = DecorateVisitor().visit(ast)
+    BigOEvaluator(bigo_ast).eval()
 
     print(bigo_ast.to_json())
 
