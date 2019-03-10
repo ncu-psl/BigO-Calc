fb4d9191d3be31e2f7d76cbdf3cb39fc89b81faf
dyhe83 <32974708+dyhe83@users.noreply.github.com>
2019-03-10 16:25:43 +0800

    Unit test & remove ast_node_decorator redundant check (#12)
    * move out unit_test folder from ast_decorator/
    
    * unit test
    
    * remove redundant check

---
+++ unit_test/ast_decorator/java/test_ast_node_decorator.py
@@ -0,0 +1,26 @@
+from unittest import TestCase
+
+
+class TestDecorator(TestCase):
+    def test_decorate(self):
+        self.fail()
+
+
+class TestFileAstDecorator(TestCase):
+    def test_decorate(self):
+        self.fail()
+
+
+class TestFuncDefDecorator(TestCase):
+    def test_decorate(self):
+        self.fail()
+
+
+class TestFuncCallDecorator(TestCase):
+    def test_decorate(self):
+        self.fail()
+
+
+class ForDecorator(TestCase):
+    def test_decorate(self):
+        self.fail()
---
+++ unit_test/bigo_calculator/test_bigo_evaluator.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+
+class TestBigOEvaluator(TestCase):
+    def test_eval(self):
+        self.fail()
+
+    def test_visit_ForNode(self):
+        self.fail()
+
+    def test_visit_FuncCallNode(self):
+        self.fail()
+
+
+class FuncDeclEvaluator(TestCase):
+    def test_visit_FuncDeclNode(self):
+        self.fail()
