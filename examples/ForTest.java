208e0842acfa2076285d66ff27c3e22923174b8b
dyhe83 <dyhe83@gmail.com>
2019-05-06 09:49:47 +0800

    rename function

--- examples/ForTest.java
+++ examples/ForTest.java
@@ -13,7 +13,7 @@ class Main {
                 k++;
     }
 
-    public static void n_2_n(int n) {
+    public static void n_2n(int n) {
         int k = 0;
         for(int i=1;i<=n;i++)
             for(int j=1;j<=1<<n;j++)
@@ -34,7 +34,7 @@ class Main {
                 k++;
     }
 
-    public static void logn_2_n(int n) {
+    public static void logn_2n(int n) {
         int k = 0;
         for(int i=1;i<=n;i*=2)
             for(int j=1;j<=1<<n;j++)
