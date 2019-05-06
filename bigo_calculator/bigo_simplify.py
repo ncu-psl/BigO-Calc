598a33d1e53d20edde88872f1604a03a1391380f
dyhe83 <dyhe83@gmail.com>
2019-05-06 10:03:04 +0800

    support Indirect function call

--- bigo_calculator/bigo_simplify.py
+++ bigo_calculator/bigo_simplify.py
@@ -7,6 +7,7 @@ from bigo_ast.bigo_ast_visitor import BigOAstVisitor
 class BigOSimplify(BigOAstVisitor):
     def __init__(self, root: BasicNode):
         self.root = root
+
         self.func_list = []
         for func in self.root.children:
             if type(func) == FuncDeclNode:
@@ -20,20 +21,24 @@ class BigOSimplify(BigOAstVisitor):
 
         pass
 
-    def recursively_simplify(self, func):
-        var_list = []
+    def recursively_simplify(self, func: FuncDeclNode):
+        # expend function call
         for symbol in func.time_complexity.free_symbols:
             target_func = self.get_function_by_name(str(symbol))
+
             if target_func:
-                self.recursively_simplify(target_func)
-                func.time_complexity = func.time_complexity.subs(symbol, target_func.time_complexity)
-            else:
-                var_list.append((symbol, sympy.oo))
+                if not target_func.recursive:
+                    target_tc = self.recursively_simplify(target_func)
 
-        for param in func.parameter:
-            var = sympy.Symbol(param.target.name)
-            var_list.append((var, sympy.oo))
+                    # substitute target function complexity into function
+                    func.time_complexity = func.time_complexity.subs(symbol, target_tc)
 
+        # get all symbol  (n, m, ...)
+        var_list = set()
+        for symbol in func.time_complexity.free_symbols:
+            var_list.update([(symbol, sympy.oo)])
+
+        # simplify time complexity
         func.time_complexity = func.time_complexity.simplify()
         if var_list:
             try:
@@ -41,11 +46,10 @@ class BigOSimplify(BigOAstVisitor):
                 func.time_complexity = sympy.O(func.time_complexity, *var_list).args[0]
             except:
                 pass
-                # logging.exception("sympy can not simplify: ", func.time_complexity)
         else:
             func.time_complexity = sympy.O(func.time_complexity, *var_list).args[0]
 
-        pass
+        return func.time_complexity
 
     def get_function_by_name(self, target_name: str) -> FuncDeclNode:
         for function in self.func_list:
