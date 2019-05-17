7bcd991cc9dfd9e9dfe9323808ced8433001e543
dyhe83 <dyhe83@gmail.com>
2019-05-17 20:41:53 +0800

    if (function, if else, for loop) does have body then body time complexity = 1

--- bigo_calculator/bigo_calculator.py
+++ bigo_calculator/bigo_calculator.py
@@ -31,6 +31,8 @@ class BigOCalculator(BigOAstVisitor):
             for child in func_decl_node.children:
                 self.visit(child)
                 tc += child.time_complexity
+            if tc == 0:
+                tc = 1
             func_decl_node.time_complexity = tc
 
         pass
@@ -97,10 +99,16 @@ class BigOCalculator(BigOAstVisitor):
             self.visit(child)
             true_tc += child.time_complexity
 
+        if true_tc == 0:
+            true_tc = 1
+
         false_tc = 0
         for child in if_node.false_stmt:
             self.visit(child)
             false_tc += child.time_complexity
+
+        if false_tc == 0:
+            false_tc = 1
 
         if_node.time_complexity = cond_tc + sympy.Max(true_tc, false_tc)
 
@@ -147,6 +155,8 @@ class BigOCalculator(BigOAstVisitor):
         for child in for_node.children:
             self.visit(child)
             tc += child.time_complexity
+        if tc == 0:
+            tc = 1
         for_node.time_complexity = step * tc
 
         pass
